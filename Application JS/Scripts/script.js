722218a5881d008db1c01c4b8904cf177344598a
Anthony Ramsay <Anthorapido@gmail.com>
2025-04-30 20:38:46 -0400

    Ajout de la page HTML, CSS et début du script.js

---
+++ Application JS/Scripts/script.js
@@ -0,0 +1,13 @@
+// SECTION CRÉATION UTILISATEUR
+let inputPrenom = document.getElementById("prenom");
+let inputNom = document.getElementById("nom");
+let inputCourriel = document.getElementById("courriel");
+let inputMDP = document.getElementById("mot_de_passe");
+let boutonCreation = document.getElementById("creer-utilisateur");
+
+// SECTION RECUPÉRATION API
+let inputCourrielRecuperation = document.getElementById("courriel_recuperation");
+let inputMDPRecuperation = document.getElementById("mot_de_passe_recuperation");
+let inputCheckbox = document.getElementById("checkbox-generer");
+let boutonRecuperation = document.getElementById("bouton-recuperer");
+
